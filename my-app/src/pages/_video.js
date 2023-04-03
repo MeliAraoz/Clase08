eed85ff4927cf76541e3dae51efdea57277c9a8b
MeliAraoz <melii.araoz96@gmail.com>
2023-04-03 18:24:28 -0300

    Componente Video

---
+++ my-app/src/pages/_video.js
@@ -0,0 +1,7 @@
+export default function Video() {
+    return(
+        <video controls width="320" height="240" autoplay>
+            <source src="https://www.w3schools.com/tags/movie.mp4" type="video/mp4"/>
+        </video>
+    )
+}
